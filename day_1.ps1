a03fbdc60beb824c8149f2252262a6bc41e0b4a5
sandertan <knutsi98@gmail.com>
2024-12-03 22:13:50 +0100

    finished day 1 part 2

--- day_1.ps1
+++ day_1.ps1
@@ -75,7 +75,7 @@ lists?
 #>
 
 # Read the input file
-$locations = Get-Content -Path .\input.txt
+$locations = Get-Content -Path .\example.txt
 
 # Split the locations into two lists
 $leftList = @()
@@ -98,3 +98,76 @@ for ($i = 0; $i -lt $leftList.Count; $i++) {
 }
 
 $distance
+
+<#
+--- Part Two ---
+
+Your analysis only confirmed what everyone feared: the two lists of location IDs are indeed very 
+different.
+
+Or are they?
+
+The Historians can't agree on which group made the mistakes or how to read most of the Chief's 
+handwriting, but in the commotion you notice an interesting detail: a lot of location IDs appear in 
+both lists! Maybe the other numbers aren't location IDs at all but rather misinterpreted 
+handwriting.
+
+This time, you'll need to figure out exactly how often each number from the left list appears in 
+the right list. Calculate a total similarity score by adding up each number in the left list after 
+multiplying it by the number of times that number appears in the right list.
+
+Here are the same example lists again:
+
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+
+For these example lists, here is the process of finding the similarity score:
+
+    The first number in the left list is 3. It appears in the right list three times, so the 
+    similarity score increases by 3 * 3 = 9.
+
+    The second number in the left list is 4. It appears in the right list once, so the similarity 
+    score increases by 4 * 1 = 4.
+    
+    The third number in the left list is 2. It does not appear in the right list, so the similarity 
+    score does not increase (2 * 0 = 0).
+    
+    The fourth number, 1, also does not appear in the right list.
+    
+    The fifth number, 3, appears in the right list three times; the similarity score increases 
+    by 9.
+    
+    The last number, 3, appears in the right list three times; the similarity score again increases 
+    by 9.
+
+So, for these example lists, the similarity score at the end of this process is 
+31 (9 + 4 + 0 + 0 + 9 + 9).
+
+Once again consider your left and right lists. What is their similarity score?
+#>
+
+# Read the input file
+$locations = Get-Content -Path .\input.txt
+
+# Split the locations into two lists
+$leftList = @()
+$rightList = @()
+
+foreach ($location in $locations) {
+    $left, $right = $location -split '\s+'
+    $leftList += $left
+    $rightList += $right
+}
+
+# Calculate the similarity score
+$similarityScore = 0
+foreach ($left in $leftList) {
+    $left = [int]$left
+    $similarityScore += $left * ($rightList | Where-Object {$_ -eq $left}).Count
+}
+
+$similarityScore
