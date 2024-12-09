9503a99732c0c268a8662ecf7fd8d69f6fa0294b
sandertan <knutsi98@gmail.com>
2024-12-09 15:34:05 +0100

    day 3 part 2 complete

--- day_3/day_3.ps1
+++ day_3/day_3.ps1
@@ -53,3 +53,70 @@ foreach ($line in $lines) {
 
 # Print the result
 $result
+
+<#
+--- Part Two ---
+
+As you scan through the corrupted memory, you notice that some of the conditional statements are 
+also still intact. If you handle some of the uncorrupted conditional statements in the program, you 
+might be able to get an even more accurate result.
+
+There are two new instructions you'll need to handle:
+
+    The do() instruction enables future mul instructions.
+    The don't() instruction disables future mul instructions.
+
+Only the most recent do() or don't() instruction applies. At the beginning of the program, mul 
+instructions are enabled.
+
+For example:
+
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+
+This corrupted memory is similar to the example from before, but this time the mul(5,5) and 
+mul(11,8) instructions are disabled because there is a don't() instruction before them. The other 
+mul instructions function normally, including the one at the end that gets re-enabled by a do() 
+instruction.
+
+This time, the sum of the results is 48 (2*4 + 8*5).
+
+Handle the new instructions; what do you get if you add up all of the results of just the enabled 
+multiplications?
+#>
+
+# Read the input file
+$lines = Get-Content -Path .\day_3\input.txt
+
+# Initialize the result
+$result = 0
+
+# Initialize the flag
+$enabled = $true
+
+# Loop through each line
+foreach ($line in $lines) {
+    # Find all the matches of the pattern
+    $matches = [regex]::Matches($line, "mul\((\d+),(\d+)\)|do\(\)|don\'t\(\)")
+
+    # Loop through each match
+    foreach ($match in $matches) {
+        if ($match.Value -eq "do()") {
+            $enabled = $true
+        } elseif ($match.Value -eq "don't()") {
+            $enabled = $false
+        } else {
+            # Check if the instruction is enabled
+            if ($enabled) {
+                # Get the two numbers
+                $num1 = [int]$match.Groups[1].Value
+                $num2 = [int]$match.Groups[2].Value
+
+                # Add the product to the result
+                $result += $num1 * $num2
+            }
+        }
+    }
+}
+
+# Print the result
+$result
