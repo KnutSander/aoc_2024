bdd3eece796e8ea99e986bf931c0ac5c75e40f86
sandertan <knutsi98@gmail.com>
2024-12-10 16:55:55 +0100

    day 5 part 2 finished, comments are lacking however

--- day_5/day_5.ps1
+++ day_5/day_5.ps1
@@ -149,7 +149,6 @@ foreach($update in $updates) {
     if($inOrder) {
         $middleIndex = [math]::Floor($update.Length / 2)
         $middlePage = $update[$middleIndex]
-        $middlePage
         $sum += $middlePage
     }
 }
@@ -157,3 +156,94 @@ foreach($update in $updates) {
 # Output result
 $sum
 
+<#
+--- Part Two ---
+
+While the Elves get to work printing the correctly-ordered updates, you have a little time to fix 
+the rest of them.
+
+For each of the incorrectly-ordered updates, use the page ordering rules to put the page numbers in 
+the right order. For the above example, here are the three incorrectly-ordered updates and their 
+correct orderings:
+
+    75,97,47,61,53 becomes 97,75,47,61,53.
+    61,13,29 becomes 61,29,13.
+    97,13,75,29,47 becomes 97,75,47,29,13.
+
+After taking only the incorrectly-ordered updates and ordering them correctly, their middle page 
+numbers are 47, 29, and 47. Adding these together produces 123.
+
+Find the updates which are not in the correct order. What do you get if you add up the middle page 
+numbers after correctly ordering just those updates?
+#>
+
+$sum = 0
+
+# Iterate over updates
+foreach($update in $updates) {
+    $update = $update -split ','
+    
+    # Check if update is in correct order
+    $inOrder = $true
+
+    foreach($rule in $rules) {
+        # Find indexes of before and after pages
+        $beforeIndex = [array]::IndexOf($update, $rule.Before)
+        $afterIndex = [array]::IndexOf($update, $rule.After)
+
+        # If before page is after after page, update is not in order
+        if($beforeIndex -ne -1 -and $afterIndex -ne -1 -and $beforeIndex -gt $afterIndex) {
+            $inOrder = $false
+            break
+        }
+    }
+
+    # If update is not in order, order it correctly
+    if(-not $inOrder) {
+        $orderedUpdate = New-Object System.Collections.ArrayList
+        $orderedUpdate.Add($update[0]) | Out-Null
+
+        # Iterate over pages in update
+        for($i = 1; $i -lt $update.Length; $i++) {
+            $beforePos = $i
+            $afterPos = 0
+            $page = $update[$i]
+
+            # Check rules for page
+            foreach($rule in $rules) {
+                
+                # Find indexes of before and after pages
+                if($rule.Before -eq $page) {
+                    $afterIndex = $orderedUpdate.IndexOf($rule.After)
+
+                    # If after page is before before page, update position
+                    if($afterIndex -ne -1 -and $afterIndex -lt $beforePos) {
+                        $beforePos = $afterIndex
+                    }
+                } elseif ($rule.After -eq $page) {
+                    $beforeIndex = $orderedUpdate.IndexOf($rule.Before)
+
+                    # If before page is after after page, update position
+                    if($beforeIndex -ne -1 -and $beforeIndex + 1 -gt $afterPos) { 
+                        $afterPos = $beforeIndex + 1
+                    }
+                }
+            }
+
+            # Insert page in correct position
+            if($beforePos -gt $afterPos) {
+                $orderedUpdate.Insert($beforePos, $page) | Out-Null 
+            } else {
+                $orderedUpdate.Insert($afterPos, $page) | Out-Null 
+            }
+        }
+
+        # Find middle page and add to sum
+        $middleIndex = [math]::Floor($orderedUpdate.Count / 2)
+        $middlePage = $orderedUpdate[$middleIndex]
+        $sum += $middlePage
+    }
+}
+
+# Output result
+$sum
